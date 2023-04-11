431b6de2b39ce9ddce6f26d40522335db138a54b
Stian Lockhart Pedersen <stilo759@student.liu.se>
2023-04-11 15:16:43 +0200

    first v2 commit

--- lab1/l1_script.R
+++ lab1/l1_script.R
@@ -1,1 +1,13 @@
 n = 70
+success = 22
+failures = n - success
+alpha = 8
+b = 8
+# Posterior = Beta(alpha + success, b + failures)
+lhs = alpha + success
+rhs = b + failures
+
+# a)
+for (n in seq(1, 10000, 1)){
+  
+}
