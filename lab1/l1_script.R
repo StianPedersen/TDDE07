240718c7fa6c921c34244856f5666dbbc51c0bb4
David Albrekt <daval064@student.liu.se>
2023-04-17 15:13:46 +0200

    dont know

--- lab1/l1_script.R
+++ lab1/l1_script.R
@@ -19,17 +19,26 @@ for (n in seq(1, nDraws, 1)){
   E[n] = mean(result)
 }
 
-# The true values E = alpha / (alpha + beta)
+
+
+
+plot(E, main = "Expected value as a function of #Draws",
+     xlab = "# of Draws",
+     ylab = "Expected Value E[theta|y]")
+# The true value E = alpha / (alpha + beta)
 # where our posterior gives alpha = lhs and beta = rhs
-lhs / (lhs+ rhs)
+lhs / (lhs+ rhs) # = 0.3488
 
-plot(E)
-
-Variance = (lhs *rhs) / (((lhs + rhs)^2) *(lhs + rhs + 1))
-SD_Calc = sqrt(Variance)
-plot(SD)
-SD_Calc
-SD[nDraws]
+plot(SD, main = "SD for different sample sizes",
+     xlab = "# of Draws",
+     ylab = "Standard deviation",
+     ylim = c(0.03,0.08))
+# The true value 
+# SD = sqrt((alpha * beta) / (((alpha + beta)^2) *(alpha + beta + 1)))
+# where our posterior gives alpha = lhs and beta = rhs
+SD_Calc = sqrt((lhs *rhs) / (((lhs + rhs)^2) *(lhs + rhs + 1)))
+SD_Calc # = 0.05109
+SD[nDraws] # = 0.05116S
 
 ##########################################################
 #B
@@ -61,4 +70,10 @@ hist(odds)
 plot(density(odds))
 
 
-
+############################################################
+##                  TASK 2                                ##
+############################################################
+n = 8 
+monthly_income = c(33, 24, 48, 32, 55, 74, 23, 17)
+mean = 3.6
+log(mean(monthly_income))
