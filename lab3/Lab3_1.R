6a53572011c6e6fc572616dcacdf02f8b0f730c7
David Albrekt <daval064@student.liu.se>
2023-05-08 18:17:00 +0200

    Task 1 completed

--- lab3/Lab3_1.R
+++ lab3/Lab3_1.R
@@ -1,12 +1,10 @@
 data <- readRDS("Precipitation.rds")
 logdata <- log(data)
 
-
-
 #Init Values
-u_0 <- 1.5
+u_0 <- 2
 t_0 <- 1
-sigma_0 <- 1.2
+sigma_0 <- 1
 v_0 <-  1
 
 #Make a function to draw u 
@@ -32,14 +30,13 @@ draw_sigma <- function(v_0, sigma_0, x, u)
   return(draw_inv_chi(v_n, scaler))
 }
 
-N <- 1000 #NMBR OF SAMPLES
-
+#NMBR OF SAMPLES
+N <- 1000
+#Start values
 u_samples <- matrix(ncol = 1, nrow = 1)
 sigma_samples <- matrix(ncol = 1, nrow = 1)
-
 u_start <- 3
 sigma_start <- sigma_0
-
 u_samples[1] <- u_0
 sigma_samples[1] <- sigma_start
 
@@ -61,32 +58,36 @@ for (i in 2:(N*2))
   }
 }
 
-plot(u_samples, sigma_samples, type = 'l')
+plot(u_samples, sigma_samples, type = 'l', main = "Gibbs sample 1000 iterations")
 acf(u_samples)
 acf(sigma_samples)
-plot(sigma_samples, type = 'l', col = 'red')
+acf_u <- acf(u_samples, plot = FALSE)
+acf_sigma2 <- acf(sigma_samples, plot = FALSE)
+IF_u <- 1 + 2 * sum(acf_u$acf[-1])
+IF_sigma2 <- 1 + 2 * sum(acf_sigma2$acf[-1])
+
+plot(sigma_samples, type = 'l', col = 'red', 
+     xlim = c(0, 1000), ylab = "Sample value",
+     main = "Trajectories")
 lines(u_samples, col = 'blue', type = 'l')
-
+legend("topright", legend = c("sigma", "U"),
+       col = c("red", "blue"), lty = 1)
 #B
-u_start <- 3
-sigma_start <- sigma_0
-
-u_samples[1] <- u_0
-sigma_samples[1] <- sigma_start
-
-#Start by sample u and sigma.
-for (i in 2:(N + 1000))
+#Discard the first 100 samples when sampling using the draws from a)
+y_draws <-  matrix(ncol = (N-100), nrow = 1)
+for (i in 1:(N - 100))
 {
-    u_samples[i] <- draw_u(u_0, t_0, sigma_samples[i-1], data_mean, data_length)
-    sigma_samples[i] <- draw_sigma(v_0, sigma_0, logdata, u_samples[i])  
-}
-
-y_draws <-  matrix(ncol = N, nrow = 1)
-for (i in 1:N)
-{
-  u <- u_samples[i + 1000]
-  sd <- sqrt(sigma_samples[i + 1000])
+  u <- u_samples[(2 * i) + 100]
+  sd <- sqrt(sigma_samples[(2 * i) + 100])
   y_draws[i] <- exp(rnorm(1, u, sd))
 }
 
-plot(y = as.dataframe(y_draws), x = length(y_draws))
+#Make a historgram, and add density curves for the data and the posterior predictions.
+hist(data, breaks = 20,
+     freq = FALSE,
+     main = "Histogram and Posterior Predictive Density",
+     xlab = "Daily Precipitation", ylim = c(0,0.15))
+lines(density(data), col = "red")
+lines(density(y_draws), col = "blue")
+legend("topright", legend = c("Observed Data", "Posterior Predictive Density"),
+       col = c("red", "blue"), lty = 1)
